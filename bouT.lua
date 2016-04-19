4eb0c2b53ef9605de7d0e5207bfb3b077f779718
Stefan <stefan@avochoc.com>
2016-04-19 09:41:22 +0200

    Updated bou to fit original UI design and work with syllables

--- bouT.lua
+++ bouT.lua
@@ -9,9 +9,9 @@ local sceneName = ...
 local composer = require( "composer" )
 local gr3
 if(grade == "1")then
-gr3 = require("gr1s")
+gr3 = require("gr1")
 else
- gr3 = require("gr2s")
+ gr3 = require("gr2")
 end
 local grTotal = gr3.total()
 -- Load scene with same root filename as this file
@@ -32,6 +32,7 @@ local colors ={{51/255, 204/255, 51/255},
 local prevWords = {}
 local canvas ={}
 local pieces = {}
+local mpieces = {}
 local tospell = {}
 local counter = 1
 local wordsGroup = display.newGroup()
@@ -66,31 +67,34 @@ end
 
 local function getNextWord()
 	local r 
-	local syllable 
+	local word 
 	local check = true
 	while(check)do
 		check = false
 		r = math.random(grTotal )
-		syllable = gr3.getSyllable(r)
+		word = gr3.getWord(r)
 		
-		
+		if(string.len(word)<3) then
+				check =true
+		end
 		for i=1,#prevWords do
-			if syllable == prevWords[i] then
+			if word == prevWords[i] then
 				print(i)
 				check =false
 			end
 		end
 	end
 	
-	--syllable = string.gsub( syllable, "%-","")
-	--syllable = string.lower( syllable )
+	word = string.gsub( word, "%-","")
+	word = string.lower( word )
 	if(#prevWords < 5) then
-		prevWords[#prevWords+1] = syllable
+		prevWords[#prevWords+1] = word
 	else
 		prevWords = {}
-		prevWords[#prevWords+1]= syllable
+		prevWords[#prevWords+1]= word
+		
 	end
-	return syllable
+	return word
 end
 
 local function hasCollided( obj1, obj2 )
@@ -108,7 +112,101 @@ local function hasCollided( obj1, obj2 )
  
     return (left or right) and (up or down)
 end
-
+local function isConsonant(letter)
+	local cons = true
+	for i=1,#vowels do
+		if(letter == vowels[i] )then
+			cons =false
+		end
+	end
+	return cons
+end
+local function splitDoubleConsonants(w)
+	local doubleC = false
+	local split1 =""
+	local split2 =""
+	local l = string.len(w)-2
+	
+	
+	if(isConsonant(w:sub(1,1))==false)then
+		if(l<=2) then
+			return
+		end
+		for i = 2,l do
+			if(isConsonant(w:sub(i,i)) and isConsonant(w:sub(i+1,i+1)))then
+				split1 = w:sub(1,i)
+				split2 = w:sub(i+1,string.len(w))
+				return split1,split2
+			end
+		end
+	else	
+		if(l<=3) then
+			return
+		end
+		for i = 3,l do
+			if(isConsonant(w:sub(i,i)) and isConsonant(w:sub(i+1,i+1)))then
+				split1 = w:sub(1,i)
+				split2 = w:sub(i+1,string.len(w))
+				return split1,split2
+			end
+		end
+	end
+	
+end
+local function splitFirstVowel(w)
+	
+	local fVowel = false
+	local split1 =""
+	local split2 =""
+	local l = string.len(w)-1
+	for i = 2,l do
+		if(isConsonant(w:sub(i,i))==false)then
+			split1 = w:sub(1,i)
+			split2 = w:sub(i+1,string.len(w))
+			return split1,split2
+		end
+	end
+end
+local function splitCheck(splitWord)
+	
+	if(string.len(splitWord)<4)then
+		pieces[#pieces+1]=splitWord
+	else
+		local s1,s2 = splitDoubleConsonants(splitWord)
+		if(s1 ~= nil and s2 ~= nil)then
+			
+			splitCheck(s1)
+			splitCheck(s2)
+		else
+			--split on first vowel
+			local s1,s2 = splitFirstVowel(splitWord)
+			if(s1 ~= nil and s2 ~= nil)then
+				splitCheck(s1)
+				splitCheck(s2)
+			end
+		end
+	end
+end
+local function splitMCheck(splitWord)
+	
+	if(string.len(splitWord)<4)then
+		mpieces[#mpieces+1]=splitWord
+	else
+		local m1,m2 = splitDoubleConsonants(splitWord)
+		if(m1 ~= nil and m2 ~= nil)then
+			
+			splitMCheck(m1)
+			splitMCheck(m2)
+		else
+			--split on first vowel
+			local m1,m2 = splitFirstVowel(splitWord)
+			if(m1 ~= nil and m2 ~= nil)then
+				splitMCheck(m1)
+				splitMCheck(m2)
+			end
+		end
+	end
+end
 local function drawLines()
 	linesGroup.anchorChildren = true
 	linesGroup.anchorX = 0.5
@@ -117,61 +215,98 @@ local function drawLines()
 	linesGroup.y = display.contentHeight - yInset *4
 	
 	local i =1
-	local c=0
-	for k=2,#word do
-		local wordSize = string.len(word[k])
+	for k=1,#pieces do
+		
+		local part = pieces[k]
+		local wordSize = string.len(part)
 		
 		for j=1,wordSize do
+			
 			local dash = display.newLine(i*(xInset),yInset*5, i*(xInset) + 15,yInset*5)
 			dash:setStrokeColor(255/255, 51/255, 204/255)
 			dash.strokeWidth = 2
 			linesGroup:insert(dash)
-			--print(word[k]:sub(j,j))
 			local options = 
 			{
 				--parent = textGroup,
-				text = word[k]:sub(j,j),     
+				text = " ",     
 				--x = 0,
 				--y = 200,
 				--width = 128,     --required for multi-line and alignment
 				font = "TeachersPet",   
-				fontSize = 28,
-				align = "left"  --new alignment parameter
+				fontSize = 20,
+				align = "right"  --new alignment parameter
 			}
 
 			local myText = display.newText( options )
 			myText.anchorX =0
 			myText.anchorY =0
-			myText.alpha = 0
+			myText.alpha = 1
 			myText.x = i*(xInset)
 			myText.y = yInset * 3.5
 			myText:setFillColor( 0, 0, 0 )
-			myText.piece = word[k]
-			myText.pos = i
+			myText.pos = k
 			tospell[i] = myText
 			linesGroup:insert(tospell[i])
 			i=i+1
 		end
-		
 	end
-	for i=1,#tospell do
-	print(tospell[i].text)
-	end
-	
 end
 
 local function Next()
 		
 		word = getNextWord()
+		mockWord = getNextWord()
+		mock2Word = getNextWord()
 		isPlaying = true
-		drawLines()
+		
 		timer.performWithDelay(500,function()
-		wordSound = audio.loadSound( "sound/graad"..grade.."/"..word[1]..".mp3" )
+		wordSound = audio.loadSound( "sound/graad"..grade.."/"..word..".mp3" )
 		wordChannel = audio.play( wordSound ,{onComplete= function() isPlaying = false end})
 		wordComplete = false
 		end)
 		--print(word)
+		local s1,s2 = splitDoubleConsonants(word)
 		
+		if(s1 ~= nil and s2 ~= nil)then
+			
+			splitCheck(s1)
+			splitCheck(s2)
+		else
+			--split on first vowel
+			local s1,s2 = splitFirstVowel(word)
+			if(s1 ~= nil and s2 ~= nil)then
+				splitCheck(s1)
+				splitCheck(s2)
+			end
+		end
+		--drawLines()
+		local m1,m2 = splitDoubleConsonants(mockWord)
+		if(m1 ~= nil and m2 ~= nil)then
+			
+			splitMCheck(m1)
+			splitMCheck(m2)
+		else
+			--split on first vowel
+			local m1,m2 = splitFirstVowel(mockWord)
+			if(m1 ~= nil and m2 ~= nil)then
+				splitMCheck(m1)
+				splitMCheck(m2)
+			end
+		end
+		local m21,m22 = splitDoubleConsonants(mock2Word)
+		if(m21 ~= nil and m22 ~= nil)then
+			
+			splitMCheck(m21)
+			splitMCheck(m22)
+		else
+			--split on first vowel
+			local m21,m22 = splitFirstVowel(mock2Word)
+			if(m21 ~= nil and m22 ~= nil)then
+				splitMCheck(m21)
+				splitMCheck(m22)
+			end
+		end
 		
 		local function drag( event )
 			if event.phase == "began" then
@@ -192,18 +327,11 @@ local function Next()
 				
 				--print(event.target.pos)
 				if(event.target.pos~= nil)then
-					for i=1,#pieces do
-						if(hasCollided(event.target,pieces[i]))then
-							print("moving piece  "..event.target.piece)
-							print("static piece  "..pieces[i].piece)
-							if(event.target.piece == pieces[i].piece)then
-								event.target.alpha = 0
-								for j=pieces[i].spos,pieces[i].epos do
-									tospell[j].alpha =1
-								end
-								collided = true
-							end
-						end
+					if(hasCollided(event.target,event.target.rectangle))then
+						event.target.alpha = 0
+						tospell[event.target.pos].alpha = 1
+						collided = true
+				
 					end
 				
 					
@@ -215,9 +343,9 @@ local function Next()
 						transition.to(event.target,{time = 500,x=markX,y=markY})
 				end
 				display.getCurrentStage():setFocus(nil)
-				
+				print(#pieces)
 				if(collided)then
-					if(counter == #word -1)then
+					if(counter == #pieces)then
 					
 						
 						wordComplete = true
@@ -227,12 +355,9 @@ local function Next()
 						wordsGroup = display.newGroup()
 						bouGroup:insert(wordsGroup)
 						pieces = {}
+						mpieces ={}
 						tospell = {}
 						canvas ={}
-						linesGroup:removeSelf()
-						linesGroup = nil
-						linesGroup = display.newGroup()
-						bouGroup:insert(linesGroup)
 						--isPlaying = true
 						Next()
 						--isPlaying = true
@@ -249,17 +374,19 @@ local function Next()
 			
 			return true
 		end
+		
 		local matchGroup = display.newGroup()
 		wordsGroup:insert(matchGroup)
 		local prevX = 0
 		local c = 0
-		local q = 1
-		for i=2,#word do
+		for i=1,#pieces do
 			local pieceGroup = display.newGroup()
+			local r = math.random(5)
+			
 			local options = 
 			{
 				--parent = textGroup,
-				text = word[i],     
+				text = pieces[i],     
 				--x = 0,
 				--y = 200,
 				--width = 128,     --required for multi-line and alignment
@@ -273,6 +400,7 @@ local function Next()
 			myText.anchorY =0
 			myText.alpha = 1
 			myText:setFillColor( 0, 0, 0 )
+			
 			local canvasCollided = false
 			while  canvasCollided==false do
 				canvasCollided = true
@@ -292,7 +420,10 @@ local function Next()
 				end
 				rect:removeSelf()
 			end
-			if(c<5)then
+		
+			
+			--
+			if(c<6)then
 				c=c+1
 				color =unpack(colors,c)
 			else
@@ -307,26 +438,14 @@ local function Next()
 			rect:setFillColor(color[1],color[2],color[3])
 			pieceGroup:insert(rect)
 			pieceGroup:insert(myText)
-			
-			pieceGroup.piece = word[i]
-			pieceGroup.pos = i - 1
+			pieceGroup.pos = i
 		
-			canvas[#canvas+1] = rect
-			--i*(xInset),yInset*5, i*(xInset) + 15,yInset*5
-			--local rect = display.newRect(prevX -5,0,length+ 10,height+10)
-			local rect = display.newRect(prevX,0,length+ 10,height+10)
+			canvas[#canvas+1] = rect 
+			local rect = display.newRect(prevX -5,0,length+ 10,height+10)
+			
 			rect.anchorX =0
 			rect.anchorY = 0.5
-			rect.alpha = 0
-			rect.isHitTestable = true
 			rect:setFillColor(color[1],color[2],color[3])
-			
-			
-			pieces[#pieces+1] = rect
-			pieces[#pieces].spos = q
-			pieces[#pieces].piece = word[i]
-			pieces[#pieces].epos = q-1 + string.len(word[i])
-			q = q + string.len(word[i])
 			pieceGroup.rectangle = rect
 			
 			matchGroup.anchorChildren = true
@@ -336,7 +455,7 @@ local function Next()
 			local options = 
 			{
 				--parent = textGroup,
-				text = word[i],     
+				text = pieces[i],     
 				--x = 0,
 				--y = 200,
 				--width = 128,     --required for multi-line and alignment
@@ -353,17 +472,83 @@ local function Next()
 			myText.x = prevX
 			myText.y = 0
 			myText:setFillColor( 0, 0, 0 )
-			--tospell[#tospell+1] = myText
+			tospell[#tospell+1] = myText
 			matchGroup:insert(myText)
 			matchGroup.x = display.contentWidth / 2
 			matchGroup.y = display.contentHeight - yInset*4
-			--i*(xInset),yInset*5, i*(xInset) + 15,yInset*5
-			prevX = prevX + length + 25
+			prevX = prevX + length + 15
 			
 			wordsGroup:insert(pieceGroup)
 			
 			pieceGroup:addEventListener( "touch", drag )
 		end
+		
+			local pieceGroup = display.newGroup()
+			for i=1,#mpieces do
+				local pieceGroup = display.newGroup()
+				local r = math.random(5)
+				
+				local options = 
+				{
+					--parent = textGroup,
+					text = mpieces[i],     
+					--x = 0,
+					--y = 200,
+					--width = 128,     --required for multi-line and alignment
+					font = "TeachersPet",   
+					fontSize = 36,
+					align = "right"  --new alignment parameter
+				}
+
+				myText = display.newText( options )
+				myText.anchorX =0
+				myText.anchorY =0
+				myText.alpha = 1
+				--
+				
+				local canvasCollided = false
+				while  canvasCollided==false do
+					canvasCollided = true
+					local xPos = math.random(display.contentWidth - xInset*10)+ xInset*5
+					local yPos =  math.random(display.contentHeight - yInset*10)+ yInset
+					myText.x =xPos
+					myText.y = yPos
+					
+					for q=1,#canvas do
+						
+						if(hasCollided(myText,canvas[q]))then
+							
+							canvasCollided = false
+						end
+					end
+					
+				end
+				myText.pos = i
+				p = i
+				myText:setFillColor( 0, 0, 0 )
+				--
+				
+				if(c<5)then
+					c=c+1
+					color =unpack(colors,c)
+				else
+					c = 1
+					color =unpack(colors,c)
+				end
+				local length = myText.contentWidth
+				local height = myText.contentHeight
+				local rect = display.newRect(myText.x-5,myText.y +height/2,length+ 10,height+10)
+				rect.anchorX =0
+				rect.anchorY = 0.5
+				rect:setFillColor(color[1],color[2],color[3])
+				pieceGroup:insert(rect)
+				pieceGroup:insert(myText)
+				wordsGroup:insert(pieceGroup)
+				canvas[#canvas+1] = rect 
+				pieceGroup:addEventListener( "touch", drag )
+			end	
+		
+		
 		
 end
 function scene:create( event )
@@ -429,7 +614,7 @@ function scene:create( event )
 			transition.to(soundButton,{time = 100, alpha = 1})
 			end})
 			if(isPlaying==false)then
-				wordSound = audio.loadSound( "sound/graad"..grade.."/"..word[1]..".mp3" )
+				wordSound = audio.loadSound( "sound/graad"..grade.."/"..word..".mp3" )
 				isPlaying = true
 				wordChannel = audio.play( wordSound ,{onComplete=function()isPlaying=false end})
 			end
@@ -465,7 +650,7 @@ function scene:show( event )
 		end
 		if(isPlaying==false)then
 			timer.performWithDelay(500,function()
-			wordSound = audio.loadSound("sound/graad"..grade.."/"..word[1]..".mp3" )
+			wordSound = audio.loadSound("sound/graad"..grade.."/"..word..".mp3" )
 			isPlaying = true
 			wordChannel = audio.play( wordSound ,{onComplete=function()isPlaying=false end})
 			end)
